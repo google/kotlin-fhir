6f2e8ad3ed56eea0dfae0bf97127c7220cdc6170
Elly Kitoto <junkmailstoelly@gmail.com>
2025-06-10 01:30:22 +0300

    Refactor implementation
    Inline code. Delete unused code and use apply scope function
    to improve code readability.
    
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- fhir-codegen/gradle-plugin/src/main/kotlin/com/google/fhir/codegen/primitives/EnumerationFileSpecGenerator.kt
+++ fhir-codegen/gradle-plugin/src/main/kotlin/com/google/fhir/codegen/primitives/EnumerationFileSpecGenerator.kt
@@ -42,101 +42,122 @@ object EnumerationFileSpecGenerator {
   private const val R5 = "r5"
 
   fun generate(packageName: String): FileSpec {
-    val version = packageName.substringAfterLast('.')
-
-    val baseClassName =
-      when (version) {
-        R4,
-        R4B -> "Element"
-        R5 -> "PrimitiveType"
-        else -> error("Enum generation is not supported for version: $version")
-      }
-    val extensionClassName = ClassName(packageName, "Extension")
-
-    // Define T with upper bound of Enum<*>
-    val enumType = ClassName("kotlin", "Enum").parameterizedBy(STAR)
-    val typeVariable = TypeVariableName("T", enumType)
-
-    val elementClassName = ClassName(packageName, "Element")
-    val toElementFunction = createToElementFunction(elementClassName)
-    val ofFunction = createOfFunction(elementClassName, typeVariable)
-    val companionObject = createCompanionObject(ofFunction)
-
-    val enumClass =
+    val enumerationTypeSpec =
       TypeSpec.classBuilder("Enumeration")
-        .addModifiers(KModifier.PUBLIC, KModifier.DATA)
-        .addTypeVariable(typeVariable)
-        .superclass(ClassName(packageName, baseClassName))
+        .addModifiers(KModifier.DATA)
         .apply {
-          if (version == R4 || version == R4B) {
-            addSuperclassConstructorParameter("id")
-            addSuperclassConstructorParameter("extension")
-          }
-        }
-        .primaryConstructor(
-          FunSpec.constructorBuilder()
-            .addParameter(
-              ParameterSpec.builder("id", STRING.copy(nullable = true))
-                .defaultValue("null")
-                .addKdoc("unique id for the element within a resource (for internal references)")
-                .build()
-            )
-            .addParameter(
-              ParameterSpec.builder(
-                  name = "extension",
-                  type =
-                    List::class.asTypeName()
-                      .parameterizedBy(extensionClassName.copy(nullable = true))
-                      .copy(nullable = true),
-                )
-                .addKdoc(
-                  """
+          val version = packageName.substringAfterLast('.')
+          val baseClassName =
+            when (version) {
+              R4,
+              R4B -> "Element"
+              R5 -> "PrimitiveType"
+              else -> error("Enum generation is not supported for version: $version")
+            }
+          // Define T with upper bound of Enum<*>
+          val typeVariable =
+            TypeVariableName("T", ClassName("kotlin", "Enum").parameterizedBy(STAR))
+          val extensionClassName = ClassName(packageName, "Extension")
+          val elementClassName = ClassName(packageName, "Element")
+          addTypeVariable(typeVariable)
+          primaryConstructor(
+            FunSpec.constructorBuilder()
+              .addParameter(
+                ParameterSpec.builder("id", STRING.copy(nullable = true))
+                  .defaultValue("null")
+                  .addKdoc("unique id for the element within a resource (for internal references)")
+                  .build()
+              )
+              .addParameter(
+                ParameterSpec.builder(
+                    name = "extension",
+                    type =
+                      List::class.asTypeName()
+                        .parameterizedBy(extensionClassName.copy(nullable = true))
+                        .copy(nullable = true),
+                  )
+                  .addKdoc(
+                    """
                     May be used to represent additional information that is not part of the basic definition of the
                     resource. To make the use of extensions safe and manageable, there is a strict set of governance 
                     applied to the definition and use of extensions.
                   """
-                    .trimIndent()
-                    .sanitizeKDoc()
-                )
-                .defaultValue("null")
-                .build()
-            )
-            .addParameter(
-              ParameterSpec.builder("value", typeVariable.copy(nullable = true))
-                .defaultValue("null")
-                .addKdoc("The actual value")
-                .build()
-            )
-            .build()
-        )
-        .addProperty(
-          PropertySpec.builder("id", STRING.copy(nullable = true))
-            .initializer("id")
-            .mutable(true)
-            .addModifiers(KModifier.OVERRIDE)
-            .build()
-        )
-        .addProperty(
-          PropertySpec.builder(
-              name = "extension",
-              type =
-                List::class.asTypeName()
-                  .parameterizedBy(extensionClassName.copy(nullable = true))
-                  .copy(nullable = true),
-            )
-            .initializer("extension")
-            .mutable(true)
-            .addModifiers(KModifier.OVERRIDE)
-            .build()
-        )
-        .addProperty(
-          PropertySpec.builder("value", typeVariable.copy(nullable = true))
-            .initializer("value")
-            .mutable(true)
-            .build()
-        )
-        .addFunction(toElementFunction)
-        .addType(companionObject)
+                      .trimIndent()
+                      .sanitizeKDoc()
+                  )
+                  .defaultValue("null")
+                  .build()
+              )
+              .addParameter(
+                ParameterSpec.builder("value", typeVariable.copy(nullable = true))
+                  .defaultValue("null")
+                  .addKdoc("The actual value")
+                  .build()
+              )
+              .build()
+          )
+          addProperty(
+            PropertySpec.builder("id", STRING.copy(nullable = true))
+              .initializer("id")
+              .mutable(true)
+              .addModifiers(KModifier.OVERRIDE)
+              .build()
+          )
+          addProperty(
+            PropertySpec.builder(
+                name = "extension",
+                type =
+                  List::class.asTypeName()
+                    .parameterizedBy(extensionClassName.copy(nullable = true))
+                    .copy(nullable = true),
+              )
+              .initializer("extension")
+              .mutable(true)
+              .addModifiers(KModifier.OVERRIDE)
+              .build()
+          )
+          addProperty(
+            PropertySpec.builder("value", typeVariable.copy(nullable = true))
+              .initializer("value")
+              .mutable(true)
+              .build()
+          )
+          superclass(ClassName(packageName, baseClassName))
+          if (version == R4 || version == R4B) {
+            addSuperclassConstructorParameter("id")
+            addSuperclassConstructorParameter("extension")
+          }
+          addFunction(
+            FunSpec.builder("toElement")
+              .returns(elementClassName.copy(nullable = true))
+              .addStatement(
+                "if (id != null || extension != null) { return Element(id, extension) }"
+              )
+              .addStatement("return null")
+              .build()
+          )
+          addType(
+            TypeSpec.companionObjectBuilder()
+              .addFunction(
+                FunSpec.builder("of")
+                  .addTypeVariable(typeVariable)
+                  .addParameter(
+                    ParameterSpec.builder("value", typeVariable.copy(nullable = true)).build()
+                  )
+                  .addParameter(
+                    ParameterSpec.builder("element", elementClassName.copy(nullable = true)).build()
+                  )
+                  .returns(
+                    ClassName("", "Enumeration").parameterizedBy(typeVariable).copy(nullable = true)
+                  )
+                  .addStatement(
+                    "return if (value == null && element == null) { null } else { Enumeration(element?.id, element?.extension, value = value) }"
+                  )
+                  .build()
+              )
+              .build()
+          )
+        }
         .addKdoc(
           """
             A FHIR Enumeration type bound to a specific set of codes. It represents a constrained code 
@@ -147,40 +168,6 @@ object EnumerationFileSpecGenerator {
         )
         .build()
 
-    return FileSpec.builder(packageName, "Enumeration").addType(enumClass).build()
-  }
-
-  private fun createToElementFunction(elementClassName: ClassName): FunSpec {
-    return FunSpec.builder("toElement")
-      .addModifiers(KModifier.PUBLIC)
-      .returns(elementClassName.copy(nullable = true))
-      .addStatement("if (id != null || extension != null) { return Element(id, extension) }")
-      .addStatement("return null")
-      .build()
-  }
-
-  private fun createOfFunction(
-    elementClassName: ClassName,
-    typeVariable: TypeVariableName,
-  ): FunSpec {
-    return FunSpec.builder("of")
-      .addModifiers(KModifier.PUBLIC)
-      .addTypeVariable(typeVariable)
-      .addParameter(ParameterSpec.builder("value", typeVariable.copy(nullable = true)).build())
-      .addParameter(
-        ParameterSpec.builder("element", elementClassName.copy(nullable = true)).build()
-      )
-      .returns(ClassName("", "Enumeration").parameterizedBy(typeVariable).copy(nullable = true))
-      .addStatement(
-        "return if (value == null && element == null) { null } else { Enumeration(element?.id, element?.extension, value = value) }"
-      )
-      .build()
-  }
-
-  private fun createCompanionObject(ofFunction: FunSpec): TypeSpec {
-    return TypeSpec.companionObjectBuilder()
-      .addModifiers(KModifier.PUBLIC)
-      .addFunction(ofFunction)
-      .build()
+    return FileSpec.builder(packageName, "Enumeration").addType(enumerationTypeSpec).build()
   }
 }
